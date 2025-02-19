59bde0ba98fa233b6459b4e46713b1016d3aaef5
BenC2026 <BCroyle1@my.gcu.edu>
2025-02-19 10:50:46 -0700

    Fixed Cannonball script

--- Assets/Whee.cs
+++ Assets/Whee.cs
@@ -6,15 +6,13 @@ public class Whee : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = new Vector3(transform.position.x - 222, transform.position.y - 11, transform.position.z - 159);
-        Debug.Log(transform.position);
         StartCoroutine(Fly());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     IEnumerator Fly()
